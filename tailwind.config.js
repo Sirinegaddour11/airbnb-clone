e0d7e1693fdb7f0b64d40db1e93f8992db8fcf30
syrina gaddour <syrinagaddour11@gmail.com>
2023-06-15 20:11:51 +0100

    add Modal

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,tsx}",
+    "./pages/**/*.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
